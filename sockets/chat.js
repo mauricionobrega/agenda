150b926142912f126dfca44776dc790372f3454f
Mauricio Silva Teixeira de Nobrega - Tibone <mauricio.nobrega@walmart.com>
2016-05-19 15:08:14 -0300

    redis impl

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,18 +1,20 @@
 module.exports = function(io) {
   var crypto = require('crypto'), // TODO: https://github.com/ncb000gt/node.bcrypt.js/
-      sockets = io.sockets,
-      onlines = {};
+      redis = require('redis').createClient(), // parameters: createClient(port, ip)
+      sockets = io.sockets;
 
   sockets.on('connection', function(client) {
     var session = client.handshake.session,
         usuario = session.usuario;
 
-    onlines[usuario.email] = usuario.email;
-
-    for (var email in onlines) {
-      client.emit('notify-onlines', email);
-      client.broadcast.emit('notify-onlines', email);
-    }
+    redis.sadd('onlines', ususario.email, function(erro) {
+      redis.smembers('onlines', function(erro, emails) {
+        emails.forEach(function(email) {
+          client.emit('notify-onlines', email);
+          client.broadcast.emit('notify-onlines', email);
+        })
+      });
+    });
 
     client.on('send-server', function(msg) {
       var sala = session.sala,
@@ -22,6 +24,7 @@ module.exports = function(io) {
           },
           formattedMsg = '<p><strong>'+usuario.nome+': </strong> '+msg+'</p>';
 
+      redis.lpush(sala, formattedMsg);
       client.broadcast.emit('new-message', data);
       sockets.in(sala).emit('send-client', formattedMsg);
     });
@@ -34,15 +37,25 @@ module.exports = function(io) {
       }
       session.sala = sala;
       client.join(sala);
+
+      var msg = '<p><strong>'+usuario.nome+':</strong> entrou.</p>';
+      redis.lpush(sala, msg, function(erro, res) {
+        redis.lrange(sala, 0, -1, function(erro, msgs) {
+          msgs.forEach(function(msg) {
+            sockets.in(sala).emit('send-client', msg);
+          });
+        });
+      });
     });
 
     client.on('disconnect', function() {
       var sala = session.sala,
           formattedMsg = '<p><strong>'+usuario.nome+': '+'</strong> saiu</p>';
 
+      redis.lpush(sala, formattedMsg);
       client.broadcast.emit('notify-offlines', usuario.email);
       sockets.in(sala).emit('send-client', formattedMsg);
-      delete onlines[usuario.email];
+      redis.srem(usuario.email);
       client.leave(session.sala);
     });
 
