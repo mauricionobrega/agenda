dc9abaa6190094966e55b6a9545516fc5b3fea6d
Mauricio Silva Teixeira de Nobrega - Tibone <mauricio.nobrega@walmart.com>
2016-05-20 18:13:50 -0300

    chat

---
+++ server.js
@@ -0,0 +1,16 @@
+var forever = require('forever-monitor'),
+    Monitor = forever.Monitor,
+    child = new Monitor('clusters.js', {
+      'max': 10,
+      'silent': true,
+      'killTree': true,
+      'logFile': 'logs/forever.log',
+      'outFile': 'logs/app.log',
+      'errFile': 'logs/error.log'
+    });
+
+child.on('exit', function() {
+  console.log('O servidor foi finalizado.');
+});
+
+child.start();
